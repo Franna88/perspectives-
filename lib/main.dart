935c518e505ef595b35288793bda859164b4088e
Keita-lee <keita@barefootbytes.com>
2025-01-09 16:14:51 +0200

    setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:perspectives/eventCreate/createEvent.dart';
@@ -24,25 +26,41 @@ import 'package:perspectives/perspectivesCreate/pages/perspectiveBackgroundImage
 
 import 'home/contentViewScreen/contentViewScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyAaNUIEDE5yJs_CIJr07GZwzT8ipDtLfY0",
+            authDomain: "perspectives-a77f1.firebaseapp.com",
+            projectId: "perspectives-a77f1",
+            storageBucket: "perspectives-a77f1.firebasestorage.app",
+            messagingSenderId: "1012343008916",
+            appId: "1:1012343008916:web:c405e0ea9aa9d9f0498e81",
+            measurementId: "G-1MGZ8MFZWE"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(
-    const MaterialApp(home: Scaffold(body: 
-    //CreateEventDate()
-    //CreateEvent()
-   // ImageMemoryUpload()
-    //AddFriends()
-    //PerspectiveBackgroundImage()
-    //UploadVideoMemory(isSingleMemory: false)
-    //VoiceRecord()
-   // ChooseMemoryFormat()
-    //MemoryDetails()
-    //
-    //ContentViewScreen())
-    //PerspectiveOptionsPopup()
-        HomePage(pageIndex: 0,),
-        )
-        ),
+    const MaterialApp(
+        home: Scaffold(
+      body:
+          //CreateEventDate()
+          //CreateEvent()
+          // ImageMemoryUpload()
+          //AddFriends()
+          //PerspectiveBackgroundImage()
+          //UploadVideoMemory(isSingleMemory: false)
+          //VoiceRecord()
+          // ChooseMemoryFormat()
+          //MemoryDetails()
+          //
+          //ContentViewScreen())
+          //PerspectiveOptionsPopup()
+          HomePage(
+        pageIndex: 0,
+      ),
+    )),
   );
 }
-
-
